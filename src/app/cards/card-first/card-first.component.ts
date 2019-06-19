c116e6cbc1598752a3b37fe19e617c9a00a7e0e2
IN\lalan.singh <lalan.singh@tavant.com>
2019-06-19 16:58:15 +0530

    code removed

--- src/app/cards/card-first/card-first.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'card-first',
-    templateUrl: './card-first.component.html',
-    styleUrls: ['./card-first.component.css']
-})
-export class CardFirstComponent {
-}
--- src/app/cards/card-second/card-second.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'card-second',
-    templateUrl: './card-second.component.html',
-    styleUrls: ['./card-second.component.css']
-})
-export class CardSecondComponent {
-}
