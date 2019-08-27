65908d787395ef3c2fa8ea8bfc29cbdb1a7845c9
IN\lalan.singh <lalan.singh@tavant.com>
2019-08-27 13:01:22 +0530

    no message

--- src/app/features/music-player/music-player.component.ts
+++ src/app/features/music-player/music-player.component.ts
@@ -16,112 +16,59 @@ export class MusicPlayerComponent implements OnDestroy {
     public maxDuration: number = 0;
     public minDuration: number = 0;
     public isPlayed: boolean = false;
-    public lapsTime: string = '0:00:00';
+    public lapsTime: string = '00:00';
     public isMoreControl: boolean = false;
     public audioSrc = '..//..//..//assets/music/';
-    @Input()
-    public showFileList: boolean = false;
-    @Input()
-    public likeIcon: boolean = true;
     // public W: Wave;
     public canvas: any;
     public canvHeight = 100;
     public canvWidth = 100;
     public fileDetails: any = {};
     public isNextAvailable: boolean;
-    public isPreviousAvailable: boolean;
+    public isPreviousAvailable: boolean = false;
     public isMute: boolean;
     public volumeRange: number = 1;
     public maxVolume: number = 10;
     public minVolume: number = 0;
     // public audioFileList: any = [];
-    public totalDuration: string = '0:00:00';
+    public totalDuration: string = '00:00';
     public activeSpeed: string = '1x';
 
     public mediaList: mediaFile[] = [];
     public sortedMediaList: mediaFile[];
-    public isSongInitialized: boolean;
     public activeRow: any;
 
     constructor(private waveService: Wave, private comonService: ComonService) {
         this.isNextAvailable = false;
-        this.isPreviousAvailable = false;
         this.isMute = false;
-        this.isSongInitialized = false;
 
         this.comonService.musicSrc.subscribe(mediaFileList => {
             this.mediaList = mediaFileList;
             this.sortedMediaList = this.mediaList.slice();
             if (mediaFileList.length !== 0) {
-                // this.initAudio(mediaFileList[0]);
-                // this.songSelected(mediaFileList[0]);
                 this.songLoad(mediaFileList[0]);
             } else {
                 this.clearAudioPlayer();
             }
         });
-        // let mediaTrackList = {};
-        // mediaTrackList['trackId'] = 0;
-        // mediaTrackList['src'] = this.audioSrc + 'file_example_MP3_700KB.mp3';
-        // mediaTrackList['mediaTitle'] = '';
-        // mediaTrackList['posterSrc'] = 'mp3poster3.jpeg';
-        // mediaTrackList['posterTitle'] = '';
-        // mediaTrackList['description'] = '';
-        // this.audioFileList.push(mediaTrackList);
-
-
-
-        // this.mediaList = [
-        //     {
-        //         trackId: 0,
-        //         mediaType: 'audio',
-        //         mediaTitle: 'Symphony 1 - Postrol',
-        //         mediaSubTitle: 'Baron Gottfried van Swieten',
-        //         description: '',
-        //         src: this.audioSrc + 'file_example_MP3_700KB.mp3',
-        //         posterSrc: 'mp3poster3.jpeg',
-        //         posterTitle: '',
-        //         time: '35 min',
-        //         publish: '1 Aug, 2019'
-        //     },
-        //     {
-        //         trackId: 1,
-        //         mediaType: 'audio',
-        //         mediaTitle: 'Symphony 2 - Postrol',
-        //         mediaSubTitle: 'Baron Gottfried van Swieten',
-        //         description: '',
-        //         src: this.audioSrc + 'SampleAudio_0.4mb.mp3',
-        //         posterSrc: '',
-        //         posterTitle: '',
-        //         time: '35 min',
-        //         publish: '1 Aug, 2019'
-        //     },
-        //     {
-        //         trackId: 2,
-        //         mediaType: 'audio',
-        //         mediaTitle: 'Symphony 3 - Postrol',
-        //         mediaSubTitle: 'Track 01',
-        //         description: 'Baron Gottfried van Swieten',
-        //         src: this.audioSrc + 'SampleAudio_0.7mb.mp3',
-        //         posterSrc: '',
-        //         posterTitle: '',
-        //         time: '35 min',
-        //         publish: '1 Aug, 2019'
-        //     }
-        // ];
-        // this.sortedMediaList = this.mediaList.slice();
     }
 
-
     private initAudio(currentTrack: any) {
-
         this.fileDetails.trackId = currentTrack.trackId;
         this.fileDetails.url = currentTrack.src;
         this.fileDetails.mediaTitle = currentTrack.mediaTitle;
         this.fileDetails.mediaSubTitle = currentTrack.mediaSubTitle;
         this.fileDetails.description = currentTrack.description;
         this.fileDetails.posterSrc = currentTrack.posterSrc;
-
+        this.checkNextPrevSong();
+        this.advAudio = new Audio();
+        this.advAudio.id = 'audio';
+        this.advAudio.src = this.fileDetails.url; //this.audioSrc + this.fileDetails.url;
+        this.advAudio.addEventListener('timeupdate', this.getLapsTime.bind(this));
+        this.waveService = new Wave();
+        this.waveService.fromElement(this.advAudio, "wave", VisualizerModel.dualbars_blocks);
+    }
+    private checkNextPrevSong() {
         let index = this.mediaList.findIndex(x => x.trackId === this.fileDetails.trackId);
         //set next song available to be play
         if ((index + 1) < (this.mediaList.length)) {
@@ -135,40 +82,26 @@ export class MusicPlayerComponent implements OnDestroy {
         } else {
             this.isPreviousAvailable = false;
         }
-
-        this.advAudio = new Audio();
-        this.advAudio.id = 'audio';
-        this.advAudio.src = this.fileDetails.url; //this.audioSrc + this.fileDetails.url;
-        this.advAudio.addEventListener('timeupdate', this.getLapsTime.bind(this));
-        this.waveService = new Wave();
-        this.waveService.fromElement(this.advAudio, "wave", VisualizerModel.dualbars_blocks);
     }
 
     ngAfterViewInit() {
         this.initAudio({});
-        // this.waveService.fromElement(this.advAudio, "wave", VisualizerModel.bars);
     }
     ngOnDestroy() {
         this.onStop();
         this.waveService.stopStream();
     }
-    // public onRemoveFile(index: any) {
-    //     let i = this.mediaList.findIndex(x => x.trackId === index);
-    //     this.mediaList.splice(i, 1);
-    // }
 
     public songSelected(songToBePlay: mediaFile) {
         this.onStop();
         this.initAudio(songToBePlay);
         this.onPlay();
-        this.isSongInitialized = true;
         this.activeRow = songToBePlay.trackId;
     }
 
     public songLoad(songToBePlay: mediaFile) {
         this.onStop();
         this.initAudio(songToBePlay);
-        this.isSongInitialized = true;
         this.activeRow = songToBePlay.trackId;
     }
 
@@ -218,7 +151,11 @@ export class MusicPlayerComponent implements OnDestroy {
         if (sec.toString().length < 2) { sec = "0" + sec };
         if (min.toString().length < 2) { min = "0" + min };
 
-        this.lapsTime = h + ":" + min + ":" + sec;
+        if (h > 0) {
+            this.lapsTime = h + ":" + min + ":" + sec;
+        } else {
+            this.lapsTime = min + ":" + sec;
+        }
         if (this.advAudio.duration === this.currentTime) {
             this.onStop();
         }
@@ -259,8 +196,11 @@ export class MusicPlayerComponent implements OnDestroy {
 
         if (sec.toString().length < 2) { sec = "0" + sec };
         if (min.toString().length < 2) { min = "0" + min };
-
-        this.totalDuration = h + ":" + min + ":" + sec;
+        if (h > 0) {
+            this.totalDuration = h + ":" + min + ":" + sec;
+        } else {
+            this.totalDuration = min + ":" + sec;
+        }
     }
     public onPause() {
         this.isPlayed = false;
