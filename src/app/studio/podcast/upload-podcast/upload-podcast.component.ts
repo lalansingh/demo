c51b0152a3927a5a99a50639bc7d9b2d2eda5ef1
IN\lalan.singh <lalan.singh@tavant.com>
2019-08-28 17:35:06 +0530

    no message

--- src/app/studio/podcast/upload-podcast/upload-podcast.component.ts
+++ src/app/studio/podcast/upload-podcast/upload-podcast.component.ts
@@ -1,5 +1,7 @@
 import { Component, ViewEncapsulation, HostListener, Inject, ElementRef, ViewChild, Input } from '@angular/core';
 import { ComonService } from 'src/app/common/services/comon-service';
+import { Wave } from 'src/app/features/wave';
+import { VisualizerModel, mediaFile } from 'src/app/features/media-file';
 
 @Component({
     selector: 'upload-podcast',
@@ -11,82 +13,153 @@ export class UploadPodcastComponent {
 
     @ViewChild('fileEvent', { static: false })
     private fileEvent: ElementRef;
-    private urls = [];
-    private lastPhoto: any = null;
+    private mediaFileList: mediaFile[] = [];
     private color = '3597ec';
     @Input()
     private checked = false;
     private disabled = false;
     public screenHeight: string;
-    constructor(private comonService: ComonService) {
+    public likeIcon: boolean = false;
+    public singleAudio: any;
+    public currentTime: number = 0;
+    public maxDuration: number = 0;
+    public minDuration: number = 0;
+    public isPlayed: boolean = false;
+    public canvas: any;
+    public canvHeight = 247;
+    public canvWidth = 700;
+    public selectedTrackId: any;
+
+    constructor(private comonService: ComonService, private waveService: Wave) {
         this.screenHeight = localStorage.getItem('windowHeight');
     }
 
     public addMore() {
         this.fileEvent.nativeElement.click();
     }
-
+    public onFileDropped(file: any) {
+        this.fileUpload(file);
+    }
     public onSelectFile(event) {
         this.fileUpload(event.target.files);
-        // if (event.target.files && event.target.files[0]) {
-        //     var filesAmount = event.target.files.length;
-        //     for (let i = 0; i < filesAmount; i++) {
-        //         var reader = new FileReader();
+    }
 
-        //         reader.onload = (event: any) => {
-        //             let url = {};
-        //             url['id'] = i;
-        //             url['file'] = event.target.result;
-        //             this.urls.push(url);
-        //             this.setLastPhoto();
-        //         }
-        //         reader.readAsDataURL(event.target.files[i]);
-        //     }
-        // }
+    private getTotalDuration(sec: any): string {
+        let h = Math.floor(sec / 3600);
+        sec = sec % 3600;
+
+        let min = Math.floor(sec / 60).toString();
+        sec = Math.floor(sec % 60).toString();
+
+        if (sec.toString().length < 2) { sec = "0" + sec };
+        if (min.toString().length < 2) { min = "0" + min };
+
+        return h + ":" + min + ":" + sec;
     }
 
     private fileUpload(files: any) {
+        let fileCount: number = 0;
         if (files && files[0]) {
-            var filesAmount = files.length;
-            for (let i = 0; i < filesAmount; i++) {
+            for (let i = 0; i < files.length; i++) {
                 var reader = new FileReader();
 
                 reader.onload = (event: any) => {
-                    let url = {};
-                    url['id'] = i;
-                    url['file'] = event.target.result;
-                    this.urls.push(url);
-                    this.setLastPhoto();
+                    let advAudio = new Audio(event.target.result);
+                    setTimeout(() => {
+                        let duration = this.getTotalDuration(advAudio.duration);
+                        advAudio.pause();
+                        advAudio.currentTime = 0;
+                        let mediaTrackList: mediaFile = {
+                            trackId: i,
+                            mediaType: 'audio',
+                            mediaTitle: 'Symphony 3 - Postrol',
+                            mediaSubTitle: 'Track 01',
+                            description: 'Baron Gottfried van Swieten',
+                            src: event.target.result,
+                            posterSrc: '',
+                            posterTitle: '',
+                            time: '35 min',
+                            publish: '1 Aug, 2019',
+                            duration: duration
+                        }
+
+                        this.mediaFileList.push(mediaTrackList);
+                        --fileCount;
+                        if (fileCount === 0) {
+                            this.loadMusic();
+                        }
+                    }, 100);
                 }
+
                 reader.readAsDataURL(files[i]);
+                fileCount++;
             }
         }
     }
 
     public onRemoveFile(index: any) {
-        let i = this.urls.findIndex(x => x.id === index);
-        this.urls.splice(i, 1);
-        this.setLastPhoto();
-    }
-    private setLastPhoto() {
-        if (this.urls.length !== 0) {
-            this.lastPhoto = this.urls[this.urls.length - 1].file;
+        if (index !== null && index !== undefined) {
+            let i = this.mediaFileList.findIndex(x => x.trackId === index);
+            this.mediaFileList.splice(i, 1);
         } else {
-            this.lastPhoto = null;
+            for (let i = 0; i < this.mediaFileList.length;) {
+                this.mediaFileList.splice(i, 1);
+            }
         }
-        this.setLayoutFlag();
+        this.waveService.stopStream();
+        this.onStop();
+        this.loadMusic();
     }
-    public removePreviewPhoto() {
-        this.lastPhoto = null;
+    private loadMusic() {
+        if (this.mediaFileList.length === 0) {
+            this.initAudio('');
+        } else {
+            this.initAudio(this.mediaFileList[0].src)
+        }
+        this.comonService.setLayout(this.mediaFileList.length === 0 ? false : true);
     }
-    public onClickPhoto(index: any) {
-        let i = this.urls.findIndex(x => x.id === index);
-        this.lastPhoto = this.urls[i].file;
+
+    public playSelectedSong(index: any) {
+        if (index === this.selectedTrackId) {
+            if (this.isPlayed) {
+                this.onPause();
+            } else {
+                this.onPlay();
+            }
+        } else {
+            this.onStop();
+            let i = this.mediaFileList.findIndex(x => x.trackId === index);
+            this.initAudio(this.mediaFileList[i].src);
+            this.onPlay();
+            this.selectedTrackId = index;
+        }
     }
-    public uploadFile(file: any) {
-        this.fileUpload(file);
+
+    public onPlay() {
+        this.singleAudio.play();
+        this.maxDuration = this.singleAudio.duration;
+        this.isPlayed = true;
     }
-    private setLayoutFlag() {
-        this.comonService.setLayout(this.urls.length === 0 ? false : true);
+    public onPause() {
+        this.isPlayed = false;
+        this.singleAudio.pause();
+    }
+    public clearSingleAudio() {
+        this.singleAudio = new Audio();
+        this.isPlayed = false;
+    }
+    public onStop() {
+        this.singleAudio.pause();
+        this.singleAudio.currentTime = 0;
+        this.isPlayed = false;
+    }
+
+    private initAudio(src: any) {
+        this.singleAudio = new Audio();
+        this.singleAudio.id = 'audio';
+        this.singleAudio.src = src;
+        // this.singleAudio.addEventListener('timeupdate', this.getLapsTime.bind(this));
+        this.waveService = new Wave();
+        this.waveService.fromElement(this.singleAudio, "wave", VisualizerModel.dualbars_blocks);
     }
 }
